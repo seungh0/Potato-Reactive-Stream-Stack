28dcaf4dd8b443b76e7c6595415c348d6f947d76
seungh0 <will.seungho@gmail.com>
2021-02-25 18:05:43 +0900

    feat: reactive MongoTemplate 테스트

---
+++ src/test/java/com/potato/domain/resume/ResumeRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.potato.domain.resume;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class ResumeRepositoryTest {
+
+	@Autowired
+	private ResumeRepository resumeRepository;
+
+	@AfterEach
+	void cleanUp() {
+		resumeRepository.deleteAll();
+	}
+
+	@Test
+	void ReactiveMongoTemplate_테스트() {
+		// given
+		String name = "강승호";
+		Resume resume = Resume.builder()
+				.name(name)
+				.build();
+		resumeRepository.save(resume).block();
+
+		// when
+		Flux<Resume> resumeFlux = resumeRepository.findAllResumes();
+
+		// then
+		StepVerifier.create(resumeFlux)
+				.assertNext(r -> assertThat(r.getName()).isEqualTo(name))
+				.expectComplete()
+				.verify();
+	}
+
+}
